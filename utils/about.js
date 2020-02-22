81148c3d2fc6189238a910c181ea5717d715b7e0
Daniel <mariandaniel2497@gmail.com>
2020-02-22 18:36:19 +0200

    ➕ Updated about me description.

--- utils/about.js
+++ utils/about.js
@@ -1,2 +1,2 @@
 exports.ABOUT_DESCRIPTION =
-  'I am a self taught web developer, I am passionate about designing and developing user-friendly websites. I have the skills to create high quality web apps or improve existing websites for individuals and organisations, using the latest.';
+  'I am a self taught web developer, I am passionate about designing and developing user-friendly websites. I have the skills to create high quality web apps or improve existing websites for individuals and organisations, using the latest technologies.';
